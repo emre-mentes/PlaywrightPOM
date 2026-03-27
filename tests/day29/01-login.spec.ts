c55d4b87d8f32845f524738f9e76e83452467e84
Emre MENTES <emreowen57@gmail.com>
2026-03-27 06:40:32 +0100

    changes

---
+++ tests/day29/01-login.spec.ts
@@ -0,0 +1,22 @@
+import { test, expect } from "@playwright/test";
+//Name: US100201_Customer_Login
+//Description:Login with Customer information
+//Acceptance Criteria: As Customer, I should be able to log in to the application
+//username: standard_user
+//password: secret_sauce
+//https://www.saucedemo.com/
+
+test("As Customer, I should be able to log in to the application", async ({
+  page,
+}) => {
+  await page.goto("https://www.saucedemo.com/");
+
+  await page.getByPlaceholder("Username").fill("standard_user");
+
+  await page.getByPlaceholder("Password").fill("secret_sauce");
+
+  await page.getByRole("button", { name: "Login" }).click();
+
+  await expect(page).toHaveURL("https://www.saucedemo.com/inventory.html");
+
+});
