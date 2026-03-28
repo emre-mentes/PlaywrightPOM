dba937f240e638c7a5e70fcd0f7ccc10908f2ce6
Emre MENTES <emreowen57@gmail.com>
2026-03-28 06:39:45 +0100

    changes

--- tests/day29/01-login.spec.ts
+++ tests/day29/01-login.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from "@playwright/test";
+import { SauceLoginPage } from "../../pages/SauceLoginPage";
 //Name: US100201_Customer_Login
 //Description:Login with Customer information
 //Acceptance Criteria: As Customer, I should be able to log in to the application
@@ -10,13 +11,15 @@ test("As Customer, I should be able to log in to the application", async ({
   page,
 }) => {
   await page.goto("https://www.saucedemo.com/");
-
   await page.getByPlaceholder("Username").fill("standard_user");
-
   await page.getByPlaceholder("Password").fill("secret_sauce");
-
   await page.getByRole("button", { name: "Login" }).click();
-
   await expect(page).toHaveURL("https://www.saucedemo.com/inventory.html");
+});
 
+test("Page Object Model Usage", async ({ page }) => {
+  const sauceLoginPage: SauceLoginPage = new SauceLoginPage(page);
+  await sauceLoginPage.gotoLoginPage();
+  await sauceLoginPage.login();
+  await expect(page).toHaveURL("https://www.saucedemo.com/inventory.html");
 });
