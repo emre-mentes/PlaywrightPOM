0446c0b162dc653aee4d54f192178a08576627a7
Emre MENTES <emreowen57@gmail.com>
2026-03-29 17:02:02 +0200

    changes

--- pages/SauceInventoryPage.ts
+++ pages/SauceInventoryPage.ts
@@ -1,16 +1,19 @@
-import { Locator, Page } from "@playwright/test";
+import { Locator, Page } from "playwright";
 
 
-export class SauceInventoryPage {
-
-    private readonly page : Page;
-    readonly inventoryItems : Locator
-
+export class SauceInventoryPage{
+    private readonly page :Page;
+    readonly inventoryItems : Locator;
 
     constructor(page:Page){
-        this.page = page;
-        this.inventoryItems = page.locator(".inventory_item")
-
+        this.page=page;
+        this.inventoryItems = page.locator(".inventory_item");
     }
+
+    async getInventoryItemsCount(){
+      return  await this.inventoryItems.count();
+    }
+
+
 
 }
