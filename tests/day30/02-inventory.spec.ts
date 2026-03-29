6255ff1f18bb5b615a06cffb32d2e9ca340d51eb
Emre MENTES <emreowen57@gmail.com>
2026-03-29 16:56:57 +0200

    changes

--- tests/day30/02-inventory.spec.ts
+++ tests/day30/02-inventory.spec.ts
@@ -1,14 +1,16 @@
 import { test, expect } from "@playwright/test";
-
-
+import { SauceLoginPage } from "../../pages/SauceLoginPage";
+import { SauceInventoryPage } from "../../pages/SauceInventoryPage";
 
 test("inventory item test", async ({ page }) => {
   // Test Senaryosu:
   // 1. Login sayfasına git
-
-  
+  const loginPage = new SauceLoginPage(page);
+  await loginPage.gotoLoginPage();
   // 2. "standard_user" kullanıcı adı ve "secret_sauce" şifresi ile giriş yap
-  // 3. Ürün listesi sayfasının (inventory) yüklendiğini doğrula
-  // 4. Sayfadaki tüm ürün elementlerini bir diziye al
-  // 5. Dizinin uzunluğunun 6 olduğunu doğrula
+  await loginPage.login();
+
+  // 3. Dizinin uzunluğunun 6 olduğunu doğrula
+  const inventoryPage = new SauceInventoryPage(page);
+  await expect(inventoryPage.inventoryItems).toHaveCount(6);
 });
