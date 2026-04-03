a7ebfe901a26abb451e27a979eca5c898edfff07
Emre MENTES <emreowen57@gmail.com>
2026-04-04 00:12:55 +0200

    changes

--- tests/day32/01-register.spec.ts
+++ tests/day32/01-register.spec.ts
@@ -1,18 +1,39 @@
 import { test, expect } from "@playwright/test";
+import { RegisterPage } from "../../pages/RegisterPage";
 
 test("Register Test", async ({ page }) => {
-  // Test Senaryosu: Yeni bir kullanıcının register formu üzerinden başarılı şekilde kayıt olması
-  
-  // Test Adımları:
-  // 1. https://ecommerce-playground.lambdatest.io/index.php?route=account/register sayfasına gidilir.
-  // 2. Faker ile rastgele kullanıcı bilgileri oluşturulur.
-  // 3. First Name alanı doldurulur.
-  // 4. Last Name alanı doldurulur.
-  // 5. E-Mail alanı doldurulur.
-  // 6. Telephone alanı doldurulur.
-  // 7. Password alanı doldurulur.
-  // 8. Password Confirm alanı aynı şifre ile doldurulur.
-  // 9. Privacy Policy kabul edilir.
-  // 10. Continue butonuna tıklanır.
-  // 11. Kayıt işleminin başarılı olduğu doğrulanır.
+
+  const registerPage = new RegisterPage(page);
+  await registerPage.goto();
+
+  const user = {
+    firstName: "Ali",
+    lastName: "Can",
+    email: "alican12347@test.com",
+    telephone: "+905559999999",
+    password: "Test123.",
+  };
+
+  await registerPage.fillForm(user);
+  await registerPage.acceptPrivacyPolicy();
+  await registerPage.clickContinueButton();
+ //bu kullanim hard code
+ // await expect(page.getByText("Your Account Has Been Created!")).toBeVisible();
+  await registerPage.assertRegistrationSuccess();
+
 });
+
+
+// Test Senaryosu: Yeni bir kullanıcının register formu üzerinden başarılı şekilde kayıt olması
+// Test Adımları:
+// 1. https://ecommerce-playground.lambdatest.io/index.php?route=account/register sayfasına gidilir.
+// 2. Faker ile rastgele kullanıcı bilgileri oluşturulur.
+// 3. First Name alanı doldurulur.
+// 4. Last Name alanı doldurulur.
+// 5. E-Mail alanı doldurulur.
+// 6. Telephone alanı doldurulur.
+// 7. Password alanı doldurulur.
+// 8. Password Confirm alanı aynı şifre ile doldurulur.
+// 9. Privacy Policy kabul edilir.
+// 10. Continue butonuna tıklanır.
+// 11. Kayıt işleminin başarılı olduğu doğrulanır.
